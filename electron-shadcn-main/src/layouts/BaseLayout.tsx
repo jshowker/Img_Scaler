e1bf1fddae4577c90b0b9c647a85658554c005ac
Jshowker <heufhrbvbirb@gmail.com>
2025-04-09 15:50:38 +0200

    commit

--- electron-shadcn-main/src/layouts/BaseLayout.tsx
+++ electron-shadcn-main/src/layouts/BaseLayout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import DragWindowRegion from "@/components/DragWindowRegion";
-import NavigationMenu from "@/components/template/NavigationMenu";
 
 export default function BaseLayout({
   children,
@@ -10,7 +9,6 @@ export default function BaseLayout({
   return (
     <>
       <DragWindowRegion title="electron-shadcn" />
-      <NavigationMenu />
       <main className="h-screen pb-20 p-2">{children}</main>
     </>
   );
